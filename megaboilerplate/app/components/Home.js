2e9cd1cb791d7c77f8751e8bd039d78bd9aa9a7d
webmaster444 <jlmobile710@gmail.com>
2017-10-05 22:39:14 +0300

    2nd upload

---
+++ megaboilerplate/app/components/Home.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { connect } from 'react-redux'
+import Messages from './Messages';
+import Header from './Header/Header';
+import Footer from './Footer';
+
+class Home extends React.Component {
+  render() {
+
+    let sidebarshown = 'page-wrapper';
+    if(this.props.token)
+      sidebarshown = 'page-wrapper sidebarshown';    
+
+    return (      
+      <div>      
+      <Header />
+      <div id="page-wrapper" className={sidebarshown} >
+        <Messages messages={this.props.messages}/>
+        <div className="row">
+          <div className="col-sm-4">
+            <div className="panel">
+              <div className="panel-body">
+                <h3>Heading</h3>
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
+                  mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna
+                  mollis euismod. Donec sed odio dui.</p>
+                <a href="#" role="button" className="btn btn-default">View details</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-4">
+            <div className="panel">
+              <div className="panel-body">
+                <h3>Heading</h3>
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
+                  mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna
+                  mollis euismod. Donec sed odio dui.</p>
+                <a href="#" role="button" className="btn btn-default">View details</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-4">
+            <div className="panel">
+              <div className="panel-body">
+                <h3>Heading</h3>
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
+                  mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna
+                  mollis euismod. Donec sed odio dui.</p>
+                <a href="#" role="button" className="btn btn-default">View details</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Footer />
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    messages: state.messages,
+    token: state.auth.token,
+    user: state.auth.user
+  };
+};
+
+export default connect(mapStateToProps)(Home);
